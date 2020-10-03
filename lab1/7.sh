5855955608c0ecae7bd7a345ea422cf8ea0b35e7
alexei <steam07151@gmail.com>
2020-10-03 13:17:07 +0300

    Last

--- lab1/7.sh
+++ lab1/7.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-grep -U -h -r -E -o "[[:alnum:]]+@[[:alnum:]]+.[[:alnum:]]+" /etc/ | tr -s '\n' ',' > emails.lst
+(grep -h -r -E -o "[[:alnum:]]+@[[:alnum:]]+.[[:alnum:]]+" /etc/ | tr -s '\n' ',' > emails.lst) 2> /dev/null
 
 echo '\n' >> emails.lst
