d5e75ed491597cf48563d7f614fda0b3d3a6fb62
alexei <steam07151@gmail.com>
2020-10-16 23:48:35 +0300

    5

---
+++ lab2/5.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+bash 4.sh
+
+awk 'BEGIN {i=0; now_ppid=0; now_art=0.0; count=0.0}{
+	if (i == 0)
+	{
+		count=1.0
+		now_ppid=$7
+		now_art+=$11
+	}
+	else if ($7 != now_ppid)
+	{
+		print "Average_Sleeping_Children_of_ParentID=" now_ppid " is " now_art/count
+		now_ppid=$7
+	}
+	else
+	{
+		count++;
+		now_art+=$11
+	}	
+	print $0
+	i++
+}' answer_4.info > answer_5.info
