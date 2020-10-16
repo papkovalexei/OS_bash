2edc1cf0191f8e36f3a7bfa256f1734816968a32
alexei <steam07151@gmail.com>
2020-10-16 22:27:44 +0300

    4

---
+++ lab2/4.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+getPath=`ps -ax | awk '{ 
+	path="/proc/" $1 
+	print path
+}'`
+
+echo "Task 4 info about proc" > answer_4.info
+
+for proc in $getPath
+do
+	get_ppid="$proc""/""status"
+	get_value="$proc""/""sched"
+
+	(awk '{
+		if ($1 == "Pid:")
+		{
+			printf "ProcessID=%d : ", $2 
+		}
+		if ($1 == "PPid:")
+		{
+			printf "Parent_ProcessID=%d : ", $2
+		}
+	}' $get_ppid >> answer_4.info) 2> /dev/null
+
+	(awk '{
+		if ($1 == "se.sum_exec_runtime")
+		{
+			sum_exec_runtime=$3
+		}
+		if ($1 == "nr_switches")
+		{
+			nr_switches=$3
+			print "Average_Running_Time=" sum_exec_runtime/nr_switches
+		}
+	}' $get_value >> answer_4.info) 2> /dev/null
+done
+
