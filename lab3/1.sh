7dd7cf7e937c5f82da24670384d44114f1116a17
alexei <steam07151@gmail.com>
2020-10-26 22:53:57 +0300

    1

---
+++ lab3/1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+(mkdir test 2> /dev/null) && (echo catalog test was created successfully > report.log) && touch 'test/'"$(date +"%d.%m.%Y_%H:%M:%S_Launch_Script")"
+
+(ping -w 1 "www.net_nikogo.ru" 2> /dev/null) || echo $(date +"%d.%m.%Y_%H:%M:%S") site does not work >> report.log
+
