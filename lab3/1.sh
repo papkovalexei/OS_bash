ea5845d743d5ecceb0873476224678703e59f85d
alexei <steam07151@gmail.com>
2020-10-29 14:26:02 +0300

    add

--- lab3/1.sh
+++ lab3/1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-(mkdir test 2> /dev/null) && (echo catalog test was created successfully > report.log) && touch 'test/'"$(date +"%d.%m.%Y_%H:%M:%S_Launch_Script")"
+(mkdir ~/labs/OS/lab3/test 2> /dev/null) && (echo catalog test was created successfully > ~/report.log) && touch 'test/'"$(date +"%d.%m.%Y_%H:%M:%S_Launch_Script")"
 
-(ping -w 1 "www.net_nikogo.ru" 2> /dev/null) || echo $(date +"%d.%m.%Y_%H:%M:%S") site does not work >> report.log
+(ping -w 1 "www.net_nikogo.ru" 2> /dev/null) || echo $(date +"%d.%m.%Y_%H:%M:%S") site does not work >> ~/report.log
 
