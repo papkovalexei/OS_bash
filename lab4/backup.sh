8fe4b3255d29f8e498b485385f4a4f607288f89c
alexei <steam07151@gmail.com>
2020-11-05 21:55:37 +0300

    create backup (a,b,c)

--- lab4/backup.sh
+++ lab4/backup.sh
@@ -1,17 +1,84 @@
 #!/bin/bash
 
+function newBackup {
+	file=$(echo "Backup-"$(date +"%Y-%m-%d-%M"))
+	mkdir ~/home/alexei/backup/$file
+
+	echo "Create: "$file >> ~/home/alexei/backup-report
+
+	for i in $(ls ~/home/alexei/source/)
+	do
+		cp ~/home/alexei/source/$i ~/home/alexei/backup/$file/$i
+		echo "Copy: " $i >> ~/home/alexei/backup-report
+	done
+}
+
 last_file=$(ls ~/home/alexei/backup | tail -1)
 
 if [[ $last_file == "" ]]
 then
-	last_file=$(echo "Backup-"$(date +"%Y-%m-%d"))
-	mkdir ~/home/alexei/backup/$last_file
-	
-	echo "Create: "$last_file >> ~/home/alexei/backup-report
+	newBackup
+else
+	file=$(echo "Backup-"$(date +"%Y-%m-%d-%M"))
 
-	for i in $(ls ~/home/alexei/source/)
-	do
-		cp ~/home/alexei/source/$i ~/home/alexei/backup/$last_file/$i
-		echo "Copy: " $i >> ~/home/alexei/backup-report
-	done
+	date1=$(echo $last_file | awk -F "-" '{print $5}')
+	date2=$(echo $file | awk -F "-" '{print $5}')
+	
+	#date1=$(date -d $(echo $last_file | awk -F "-" '{print $2"-"$3"-"$4}') +%s)
+	#date2=$(date -d $(echo $file | awk -F "-" '{print $2"-"$3"-"$4}') + %s)
+
+	delta=$(echo $date2"-"$date1 | bc)
+
+	#delta=$(( (date2 - date1) / 86400 ))
+
+	if [[ $delta -ge 5 ]]
+	then
+		newBackup
+	else
+		flag="0"
+
+		touch buffer1
+		touch buffer2
+
+		for i in $(ls ~/home/alexei/source/)
+		do
+			find ~/home/alexei/backup/$last_file/$i -type f 2> /dev/null > /dev/null
+
+			if [[ $? == "1" ]]
+			then
+				if [[ $flag == "0" ]]
+				then
+					echo "Update: "$last_file  $(date +"%Y-%m-%d-%M") >> ~/home/alexei/backup-report
+					flag="1"
+				fi
+
+				cp ~/home/alexei/source/$i ~/home/alexei/backup/$last_file/$i
+				echo "Copy: " $i >> buffer1
+			else
+				if [[ $flag == "0" ]]
+				then
+					echo "Update: "$last_file  $(date +"%Y-%m-%d-%M") >> ~/home/alexei/backup-report
+					flag="1"
+				fi
+
+				size1=$(wc -c ~/home/alexei/backup/$last_file/$i | awk '{print $1}')
+				size2=$(wc -c ~/home/alexei/source/$i | awk '{print $1}')
+
+				if [[ $size1 -ne $size2 ]]
+				then
+					mv ~/home/alexei/backup/$last_file/$i ~/home/alexei/backup/$last_file/$i"."$(date +"%Y-%m-%d-%M")
+					cp ~/home/alexei/source/$i ~/home/alexei/backup/$last_file/$i
+
+					echo "New: "$i  $i"."$(date +"%Y-%m-%d-%M") >> buffer2
+				fi
+			fi
+		done
+
+		cat buffer1 >> ~/home/alexei/backup-report
+		cat buffer2 >> ~/home/alexei/backup-report
+
+		rm buffer1
+		rm buffer2
+	fi
+
 fi
