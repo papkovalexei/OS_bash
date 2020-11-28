5fff9512dd578a72a54646682bf62550b9c6dcd3
alexei <steam07151@gmail.com>
2020-11-28 12:39:48 +0300

    add

---
+++ lab5/mem.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+rm report.log
+
+count_step=0
+array_index=0
+array=()
+
+while true
+do
+
+	for (( count=0; count<10; count++ ))
+	do
+		array[$array_index]=$count
+		array_index=$((array_index+1))
+	done
+
+	count_step=$((count_step+1))
+	check=$(echo $count_step"%100000" | bc)
+
+	if [[ $check -e "0" ]]
+	then
+		echo "${#array[*]}" >> report.log
+	fi
+done
